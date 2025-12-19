2e516e8467157462a6322f0b3efdb557d004e6b5
Rafael Rocha <rafaelrochasouza@outlook.com.br>
2025-12-18 23:19:12 -0300

    add: base NBs handler

--- cmd/app.go
+++ cmd/app.go
@@ -1,28 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-
-	"gerrit.o-ran-sc.org/r/ric-plt/alarm-go.git/alarm"
-	"gerrit.o-ran-sc.org/r/ric-plt/xapp-frame/pkg/clientmodel"
 	"gerrit.o-ran-sc.org/r/ric-plt/xapp-frame/pkg/xapp"
+	"github.com/RafaelRochaS/xapp-kpm-go/cmd/handlers"
 )
 
 type App struct {
-	Stats map[string]xapp.Counter
+	Stats      map[string]xapp.Counter
+	NBsHandler handlers.NBsHandler
 }
-
-var (
-	funId               = int64(1)
-	actionId            = int64(1)
-	actionType          = "report"
-	subsequestActioType = "continue"
-	timeToWait          = "w10ms"
-	xappEventInstanceID = int64(1234)
-	hPort               = int64(8080)
-	rPort               = int64(4560)
-	clientEndpoint      = clientmodel.SubscriptionParamsClientEndpoint{Host: "service-ricxapp-kpm-rmr.ricxapp", HTTPPort: &hPort, RMRPort: &rPort}
-)
 
 func main() {
 	metrics := []xapp.CounterOpts{
@@ -32,8 +18,12 @@ func main() {
 		},
 	}
 
+	nbsHandler := handlers.NewNBsImpl()
+
 	app := App{
-		Stats: xapp.Metric.RegisterCounterGroup(metrics, "kpm_app")}
+		Stats:      xapp.Metric.RegisterCounterGroup(metrics, "kpm_app"),
+		NBsHandler: nbsHandler,
+	}
 
 	app.Run()
 }
@@ -55,68 +45,16 @@ func (e *App) ConfigChangeHandler(f string) {
 func (e *App) xAppStartCB(d interface{}) {
 	xapp.Logger.Info("xApp ready call back received", d)
 
-	nbList := e.getnbList()
+	err := e.NBsHandler.SubscribeToAll()
 
-	for _, nb := range nbList {
-		e.sendSubscription(nb.InventoryName)
+	if err != nil {
+		xapp.Logger.Error("Failed to subscribe to all NBs: %v", err)
 	}
 }
 
 func (e *App) handleRICIndication(ranName string, r *xapp.RMRParams) {
 	xapp.Logger.Info("handleRICIndication", ranName, "\tparams: ", *r)
 	e.Stats["RICIndicationRx"].Inc()
-}
-
-func (e *App) sendSubscription(meid string) {
-
-	xapp.Logger.Info("sending subscription request for meid : %s", meid)
-
-	subscriptionParams := clientmodel.SubscriptionParams{
-		ClientEndpoint: &clientEndpoint,
-		Meid:           &meid,
-		RANFunctionID:  &funId,
-		SubscriptionDetails: clientmodel.SubscriptionDetailsList([]*clientmodel.SubscriptionDetail{
-			{
-				ActionToBeSetupList: clientmodel.ActionsToBeSetup{
-					&clientmodel.ActionToBeSetup{
-						ActionDefinition: clientmodel.ActionDefinition([]int64{1, 2, 3, 4}),
-						ActionID:         &actionId,
-						ActionType:       &actionType,
-						SubsequentAction: &clientmodel.SubsequentAction{
-							SubsequentActionType: &subsequestActioType,
-							TimeToWait:           &timeToWait,
-						},
-					},
-				},
-				EventTriggers:       clientmodel.EventTriggerDefinition([]int64{1, 2, 3, 4}),
-				XappEventInstanceID: &xappEventInstanceID,
-			},
-		}),
-	}
-
-	b, err := json.MarshalIndent(subscriptionParams, "", "  ")
-
-	if err != nil {
-		xapp.Logger.Error("Json marshaling failed : %s", err)
-		return
-	}
-
-	xapp.Logger.Info("*****body: %s ", string(b))
-
-	resp, err := xapp.Subscription.Subscribe(&subscriptionParams)
-
-	if err != nil {
-		xapp.Logger.Error("subscription failed (%s) with error: %s", meid, err)
-
-		// subscription failed, raise alarm
-		err := xapp.Alarm.Raise(8086, alarm.SeverityCritical, meid, "subscriptionFailed")
-		if err != nil {
-			xapp.Logger.Error("Raising alarm failed with error %v", err)
-		}
-
-		return
-	}
-	xapp.Logger.Info("Successfully subcription done (%s), subscription id : %s", meid, *resp.SubscriptionID)
 }
 
 func (e *App) Consume(msg *xapp.RMRParams) (err error) {
@@ -141,48 +79,4 @@ func (e *App) Consume(msg *xapp.RMRParams) (err error) {
 		msg.Mbuf = nil
 	}()
 	return
-}
-
-func (e *App) getnbList() []*xapp.RNIBNbIdentity {
-	var nbs []*xapp.RNIBNbIdentity
-
-	if enbs, err := e.getEnbList(); err == nil {
-		nbs = append(nbs, enbs...)
-	}
-
-	if gnbs, err := e.getGnbList(); err == nil {
-		nbs = append(nbs, gnbs...)
-	}
-
-	return nbs
-}
-
-func (e *App) getEnbList() ([]*xapp.RNIBNbIdentity, error) {
-	enbs, err := xapp.Rnib.GetListEnbIds()
-
-	if err != nil {
-		xapp.Logger.Error("err: %s", err)
-		return nil, err
-	}
-
-	xapp.Logger.Info("List of connected eNBs :")
-	for index, enb := range enbs {
-		xapp.Logger.Info("%d. enbid: %s", index+1, enb.InventoryName)
-	}
-	return enbs, nil
-}
-
-func (e *App) getGnbList() ([]*xapp.RNIBNbIdentity, error) {
-	gnbs, err := xapp.Rnib.GetListGnbIds()
-
-	if err != nil {
-		xapp.Logger.Error("err: %s", err)
-		return nil, err
-	}
-
-	xapp.Logger.Info("List of connected gNBs :")
-	for index, gnb := range gnbs {
-		xapp.Logger.Info("%d. gnbid : %s", index+1, gnb.InventoryName)
-	}
-	return gnbs, nil
 }
---
+++ cmd/handlers/nbs.handler.go
@@ -0,0 +1,148 @@
+package handlers
+
+import (
+	"encoding/json"
+
+	"gerrit.o-ran-sc.org/r/ric-plt/alarm-go.git/alarm"
+	"gerrit.o-ran-sc.org/r/ric-plt/xapp-frame/pkg/clientmodel"
+	"gerrit.o-ran-sc.org/r/ric-plt/xapp-frame/pkg/xapp"
+)
+
+type NBsHandler interface {
+	SubscribeToAll() error
+}
+
+type NBsImpl struct {
+	Nbs []*xapp.RNIBNbIdentity
+}
+
+var (
+	funId               = int64(1)
+	actionId            = int64(1)
+	actionType          = "report"
+	subsequestActioType = "continue"
+	timeToWait          = "w10ms"
+	xappEventInstanceID = int64(1234)
+	hPort               = int64(8080)
+	rPort               = int64(4560)
+	clientEndpoint      = clientmodel.SubscriptionParamsClientEndpoint{Host: "service-ricxapp-kpm-rmr.ricxapp", HTTPPort: &hPort, RMRPort: &rPort}
+)
+
+func NewNBsImpl() *NBsImpl {
+	nbsImpl := &NBsImpl{}
+
+	nbsImpl.Nbs = nbsImpl.getNBs()
+
+	return nbsImpl
+}
+
+func (n *NBsImpl) getNBs() []*xapp.RNIBNbIdentity {
+	var nbs []*xapp.RNIBNbIdentity
+
+	if enbs, err := n.getEnbList(); err == nil {
+		nbs = append(nbs, enbs...)
+	}
+
+	if gnbs, err := n.getGnbList(); err == nil {
+		nbs = append(nbs, gnbs...)
+	}
+
+	return nbs
+}
+
+func (n *NBsImpl) getEnbList() ([]*xapp.RNIBNbIdentity, error) {
+	enbs, err := xapp.Rnib.GetListEnbIds()
+
+	if err != nil {
+		xapp.Logger.Error("err: %s", err)
+		return nil, err
+	}
+
+	xapp.Logger.Info("List of connected eNBs :")
+	for index, enb := range enbs {
+		xapp.Logger.Info("%d. enbid: %s", index+1, enb.InventoryName)
+	}
+	return enbs, nil
+}
+
+func (n *NBsImpl) getGnbList() ([]*xapp.RNIBNbIdentity, error) {
+	gnbs, err := xapp.Rnib.GetListGnbIds()
+
+	if err != nil {
+		xapp.Logger.Error("err: %s", err)
+		return nil, err
+	}
+
+	xapp.Logger.Info("List of connected gNBs :")
+	for index, gnb := range gnbs {
+		xapp.Logger.Info("%d. gnbid : %s", index+1, gnb.InventoryName)
+	}
+	return gnbs, nil
+}
+
+func (n *NBsImpl) SubscribeToAll() error {
+	var err error
+
+	for _, nb := range n.Nbs {
+		err = n.sendSubscription(nb.InventoryName)
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (n *NBsImpl) sendSubscription(meid string) error {
+
+	xapp.Logger.Info("sending subscription request for meid : %s", meid)
+
+	subscriptionParams := clientmodel.SubscriptionParams{
+		ClientEndpoint: &clientEndpoint,
+		Meid:           &meid,
+		RANFunctionID:  &funId,
+		SubscriptionDetails: clientmodel.SubscriptionDetailsList([]*clientmodel.SubscriptionDetail{
+			{
+				ActionToBeSetupList: clientmodel.ActionsToBeSetup{
+					&clientmodel.ActionToBeSetup{
+						ActionDefinition: clientmodel.ActionDefinition([]int64{1, 2, 3, 4}),
+						ActionID:         &actionId,
+						ActionType:       &actionType,
+						SubsequentAction: &clientmodel.SubsequentAction{
+							SubsequentActionType: &subsequestActioType,
+							TimeToWait:           &timeToWait,
+						},
+					},
+				},
+				EventTriggers:       clientmodel.EventTriggerDefinition([]int64{1, 2, 3, 4}),
+				XappEventInstanceID: &xappEventInstanceID,
+			},
+		}),
+	}
+
+	b, err := json.MarshalIndent(subscriptionParams, "", "  ")
+
+	if err != nil {
+		xapp.Logger.Error("Json marshaling failed : %s", err)
+		return err
+	}
+
+	xapp.Logger.Info("*****body: %s ", string(b))
+
+	resp, err := xapp.Subscription.Subscribe(&subscriptionParams)
+
+	if err != nil {
+		xapp.Logger.Error("subscription failed (%s) with error: %s", meid, err)
+
+		// subscription failed, raise alarm
+		err := xapp.Alarm.Raise(8086, alarm.SeverityCritical, meid, "subscriptionFailed")
+		if err != nil {
+			xapp.Logger.Error("Raising alarm failed with error %v", err)
+		}
+
+		return err
+	}
+	xapp.Logger.Info("Successfully subcription done (%s), subscription id : %s", meid, *resp.SubscriptionID)
+
+	return nil
+}
